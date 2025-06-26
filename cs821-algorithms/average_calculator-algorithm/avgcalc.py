7eff2868be9860b5e7f1940233892a47e47b7cc6
bett kipkemoi <bettkipkemoo@gmail.com>
2025-06-26 22:39:28 +0300

    avg calc algo

---
+++ cs821-algorithms/average_calculator-algorithm/avgcalc.py
@@ -0,0 +1,14 @@
+declare scores: array(n) of integers
+declare total: integer
+declare average: real
+
+scores <- []
+
+total <- calculate total
+average <- 0.0
+for i in range(1:len(scores)):
+    total = total +scores(i)
+
+    average = total/len(scores)
+
+print(average)
